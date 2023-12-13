ada4d08be66ae1866792e7517d63037980b87010
me1ting <52079867+me1ting@users.noreply.github.com>
2023-12-13 18:30:29 +0800

    feat: rows include _rid; fields in row are sorted

--- export/json.go
+++ export/json.go
@@ -7,22 +7,24 @@ import (
 
 	"github.com/cn-poe-community/poedat/dat"
 	"github.com/cn-poe-community/poedat/schema"
+	"github.com/emirpasic/gods/maps/linkedhashmap"
 )
 
 func DatToJSON(datFile *dat.DatFile, shecmaFile *schema.SchemaFile, tableName string) []byte {
 	headers := ImportHeaders(tableName, datFile, shecmaFile)
 	rows := ExportAllRows(headers, datFile)
 
-	rowMaps := []map[string]dat.FieldValue{}
+	rowMaps := []*linkedhashmap.Map{}
 
-	for _, row := range rows {
-		rowMap := map[string]dat.FieldValue{}
+	for rid, row := range rows {
+		rowMap := linkedhashmap.New()
+		rowMap.Put("_rid", rid)
 		for i, header := range headers {
 			name := header.Name
 			if len(name) == 0 {
 				name = fmt.Sprintf("Unknown %d", i)
 			}
-			rowMap[name] = row[i]
+			rowMap.Put(name, row[i])
 		}
 
 		rowMaps = append(rowMaps, rowMap)
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/cn-poe-community/poedat
 go 1.20
 
 require golang.org/x/text v0.14.0
+
+require github.com/emirpasic/gods v1.18.1 // indirect
